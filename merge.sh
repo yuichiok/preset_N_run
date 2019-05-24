175ee8c26cfa48600ce557ab619a909d12c781b3
OKUGAWA Yuichi <yokugawa@cw06.cc.kek.jp>
2019-05-24 17:35:01 +0900

    first commit

---
+++ merge.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+	echo "Please enter isolated lepton type."
+	exit 1
+fi
+
+model=$1
+isolep=$2
+dir=/hsm/ilc/users/yokugawa/preset_N_run/${model}/${isolep}
+
+if [ ! -d "$dir" ]; then
+	echo "The directory does not exist."
+	exit 1
+else
+	echo ${dir}
+	hadd -f ${dir}/QQbarProcessor_out/root_merge/IsoLepTagged.eL.pR_${isolep}_QQbar.root ${dir}/QQbarProcessor_out/*.root
+	hadd -f ${dir}/TrashRecoProcessor_out/before_vtx_recovery/root_merge/IsoLepTagged.eL.pR_${isolep}_TRP_before.root ${dir}/TrashRecoProcessor_out/before_vtx_recovery/*.root
+	hadd -f ${dir}/TrashRecoProcessor_out/after_vtx_recovery/root_merge/IsoLepTagged.eL.pR_${isolep}_TRP_after.root ${dir}/TrashRecoProcessor_out/after_vtx_recovery/*.root
+	hadd -f ${dir}/particle_tagger_out/root_merge/IsoLepTagged.eL.pR_${isolep}_PT.root ${dir}/particle_tagger_out/*.root
+	hadd -f ${dir}/truth_vertex_finder_out/root_merge/IsoLepTagged.eL.pR_${isolep}_TVF.root ${dir}/truth_vertex_finder_out/*.root
+	hadd -f ${dir}/vertex_restorer_out/root_merge/IsoLepTagged.eL.pR_${isolep}_VR.root ${dir}/vertex_restorer_out/*.root
+fi
+
---
+++ run.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+inputxml=preset_N_run.xml
+
+# small or large
+#model=l5
+model=s5
+
+gearxml=gear_ILD_${model}_v02.xml
+
+# yyxyev
+#processID=108670
+# yyxylv
+#processID=108675
+
+# eLpR
+#processID=eL.pR
+#processID=I108670
+
+# type isolated lepton
+#isolep=electron
+isolep=muon
+#isolep=tau
+#isolep=muon_tau
+
+
+##===== New sample (large, new vertexing) =====##
+
+dirin=/hsm/ilc/users/yokugawa/
+processID=default
+
+if [ "$model" == "l5" ]; then
+	if [ "$isolep" == "electron" ]; then
+		dirin=/hsm/ilc/users/yonamine/physics/lcfiplus/vertexing_6f_ttbar/
+		processID=I108670
+	else
+		dirin=/home/ilc/yonamine/work/Yuichi/test20190328/run/slcio/
+		processID=eL.pR
+	fi
+elif [ "$model" == "s5" ]; then
+	if [ "$isolep" == "electron" ]; then
+		dirin=/hsm/ilc/users/yonamine/physics/lcfiplus/vertexing_6f_ttbar_s5/
+		processID=I108670
+	else
+		dirin=/home/ilc/yonamine/work/Yuichi/test20190328/run_s5/slcio/
+		processID=eL.pR
+	fi
+fi
+
+
+# electron sample
+#dirin=/hsm/ilc/users/yonamine/physics/lcfiplus/vertexing_6f_ttbar/
+
+# muon and tau sample
+#dirin=/home/ilc/yonamine/work/Yuichi/test20190328/run/slcio/
+
+nfile=-1
+#nfile=12
+#nfile=1
+#nperjob=50
+nperjob=3
+./submit2 $dirin $processID 1 $nfile $nperjob $inputxml $gearxml $isolep $model
